e8bb3c2356dcf17e8001d515972b59f79b464637
jeanbauerc@thoughtworks.com <jbauer@thoughtworks.com>
2019-09-02 13:27:25 -0300

    Add React testing library :heavy_plus_sign:

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from '@testing-library/react'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+describe('App', () => {
+  it('renders content', () => {
+    const { getByText } = render(<App />)
+
+    const content = getByText(
+      'Hello world'
+    )
+
+    expect(content).toBeInTheDocument()
+  })
 })
